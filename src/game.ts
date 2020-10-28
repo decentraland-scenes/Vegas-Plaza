7bab2e08c6b7bbf3f688ae2496ab154d21e6aeb7
Shibu <mshibuu@gmail.com>
2020-10-28 09:48:24 -0300

    add base models

--- src/game.ts
+++ src/game.ts
@@ -14,6 +14,143 @@ plaza.addComponent(
 
 engine.addEntity(plaza)
 
+//add VegasPlaza Stage
+let stage = new Entity()
+stage.addComponent(new GLTFShape('models/VegasPlaza_Stage.glb'))
+stage.addComponent(
+  new Transform({
+    position: new Vector3(160, 0, 160)
+  })
+)
+
+engine.addEntity(stage)
+
+//add SpiderWeb_Light_01
+let spiderweb_01 = new Entity()
+spiderweb_01.addComponent(new GLTFShape('models/SpiderWeb_Light_01.glb'))
+spiderweb_01.addComponent(
+  new Transform({
+    position: new Vector3(160, 0, 160)
+  })
+)
+
+engine.addEntity(spiderweb_01)
+
+//add SpiderWeb_Light_02
+let spiderweb_02 = new Entity()
+spiderweb_02.addComponent(new GLTFShape('models/SpiderWeb_Light_02.glb'))
+spiderweb_02.addComponent(
+  new Transform({
+    position: new Vector3(160, 0, 160)
+  })
+)
+
+engine.addEntity(spiderweb_02)
+
+//add Skeleton
+let skeleton = new Entity()
+skeleton.addComponent(new GLTFShape('models/skeleton.glb'))
+skeleton.addComponent(
+  new Transform({
+    position: new Vector3(160, 1.7, 65),
+    scale: new Vector3 (3,3,3)
+  })
+)
+
+let animator = new Animator()
+skeleton.addComponent(animator)
+let clipThriller = new AnimationState("thriller_base", {speed: 0.5})
+
+animator.addClip(clipThriller)
+clipThriller.play()
+
+engine.addEntity(skeleton)
+
+//add skeleton2  2
+let skeleton2 = new Entity()
+skeleton2.addComponent(new GLTFShape('models/skeleton.glb'))
+skeleton2.addComponent(
+  new Transform({
+    position: new Vector3(155, 1.7, 61),
+    scale: new Vector3 (3,3,3)
+  })
+)
+
+let animator2 = new Animator()
+skeleton2.addComponent(animator)
+let clipThriller2 = new AnimationState("thriller_base", {speed: 0.5})
+
+animator.addClip(clipThriller2)
+clipThriller2.play()
+
+engine.addEntity(skeleton2)
+
+//add skeleton3  2
+let skeleton3 = new Entity()
+skeleton3.addComponent(new GLTFShape('models/skeleton.glb'))
+skeleton3.addComponent(
+  new Transform({
+    position: new Vector3(165, 1.75, 61),
+    scale: new Vector3 (3,3,3)
+  })
+)
+
+let animator3 = new Animator()
+skeleton3.addComponent(animator)
+let clipThriller3 = new AnimationState("thriller_base", {speed: 0.5})
+
+animator.addClip(clipThriller3)
+clipThriller3.play()
+
+engine.addEntity(skeleton3)
+
+//add skeleton_02
+let skeleton_02 = new Entity()
+skeleton_02.addComponent(new GLTFShape('models/skeleton_02.glb'))
+skeleton_02.addComponent(
+  new Transform({
+    position: new Vector3(124, 7, 56),
+    scale: new Vector3 (5,5,5)
+  })
+)
+
+engine.addEntity(skeleton_02)
+
+//add skeleton_03
+let skeleton_03 = new Entity()
+skeleton_03.addComponent(new GLTFShape('models/skeleton_02.glb'))
+skeleton_03.addComponent(
+  new Transform({
+    position: new Vector3(196, 7, 56),
+    scale: new Vector3 (5,5,5)
+  })
+)
+
+engine.addEntity(skeleton_03)
+
+//add ram
+let ram = new Entity()
+ram.addComponent(new GLTFShape('models/ram.glb'))
+ram.addComponent(
+  new Transform({
+    position: new Vector3(160, 0, 160)
+  })
+)
+
+engine.addEntity(ram)
+
+//add light_bottom
+let light_bottom = new Entity()
+light_bottom.addComponent(new GLTFShape('models/lights_bottom.glb'))
+light_bottom.addComponent(
+  new Transform({
+    position: new Vector3(160, 0, 160)
+  })
+)
+
+engine.addEntity(light_bottom)
+
+
 //add FortuneCat
 let FortuneCat = new Entity()
 FortuneCat.addComponent(new GLTFShape('models/FortuneCat.glb'))
@@ -149,3 +286,23 @@ overalyUI([instanceSlots, instanceRoulette])
 
 // //add entity to engine
 // engine.addEntity(casinoUITrigger)
+
+Input.instance.subscribe('BUTTON_DOWN', ActionButton.PRIMARY, false, (e) => {
+  log(
+    `new Vector3(`,
+    Camera.instance.position.x,
+    ',',
+    Camera.instance.position.y,
+    ',',
+    Camera.instance.position.z,
+    `),
+	rotation: Quaternion.Euler(`,
+    0,
+    ',',
+    Camera.instance.rotation.eulerAngles.y,
+    ',',
+    0,
+    `),
+    },`
+  )
+})
